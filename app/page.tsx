11a28d6b03316308ca9c31c68d9c5a1e2e796fd8
Tim Reber <metimreber@gmail.com>
2025-11-21 20:55:13 +0100

    chore: redesign home page

--- app/page.tsx
+++ app/page.tsx
@@ -12,69 +12,74 @@ const activeStreakData: StreakData = {
       date: new Date("2025-11-18"),
       completed: true,
       totalDoses: 3,
-      missedDoses: 0
+      missedDoses: 0,
     },
     tuesday: {
       date: new Date("2025-11-19"),
       completed: true,
       totalDoses: 3,
-      missedDoses: 0
+      missedDoses: 0,
     },
     wednesday: {
       date: new Date("2025-11-20"),
       completed: true,
       totalDoses: 3,
-      missedDoses: 1
+      missedDoses: 1,
     },
     thursday: {
       date: new Date("2025-11-21"),
       completed: false,
       totalDoses: 3,
-      missedDoses: 0
+      missedDoses: 0,
     },
     friday: {
       date: new Date("2025-11-22"),
       completed: false,
       totalDoses: 3,
-      missedDoses: 0
+      missedDoses: 0,
     },
     saturday: {
       date: new Date("2025-11-23"),
       completed: false,
       totalDoses: 2,
-      missedDoses: 0
+      missedDoses: 0,
     },
     sunday: {
       date: new Date("2025-11-24"),
       completed: false,
       totalDoses: 2,
-      missedDoses: 0
-    }
-  }
+      missedDoses: 0,
+    },
+  },
 };
 
 export default async function Home() {
   const user = await getCurrentUser();
 
   if (!user) {
-    redirect('/auth/login');
+    redirect("/auth/login");
   }
 
   return (
     <>
-      <div className="bg-[#C89E85] rounded-3xl text-white m-2 p-4 relative overflow-hidden">
-        <div className="flex flex-col items-center justify-center z-30 relative">
-          <p className="font-semibold text-2xl my-5">Hallo, {user.name || 'User'}!</p>
+      <div className="bg-slate-50 rounded-3xl m-2 p-4 relative overflow-hidden">
+        <div className="flex flex-col justify-center m-2 z-30 relative">
+          <p className="font-bold text-2xl my-5">Hi, {user.name || "User"}</p>
         </div>
         <div className="relative mt-4">
-
           <div className="relative z-10 mt-8">
             <NextReminder />
           </div>
         </div>
-        <Image src="/medicinehand.png" alt="medicinehand picture" className="transform scale-x-[-1] absolute top-2 right-0 z-20 pointer-events-none" width={150} height={150} />
+        <Image
+          src="/medicinehand.png"
+          alt="medicinehand picture"
+          className="transform scale-x-[-1] absolute top-2 right-0 z-20 pointer-events-none"
+          width={150}
+          height={150}
+        />
       </div>
       <StreakWidget input={activeStreakData} />
     </>
-  )
-} 
+  );
+}
