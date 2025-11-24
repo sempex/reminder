11ee5cf0d701358ec18811a85f4a1d2a01c1b4b1
Tim Reber <metimreber@gmail.com>
2025-11-24 12:19:32 +0100

    fix: landing page

--- app/components/StreakWidget.tsx
+++ app/components/StreakWidget.tsx
@@ -25,43 +25,43 @@ export interface StreakData {
 
 export default function StreakWidget({ input }: { input: StreakData }) {
   return (
-    <div className="bg-slate-50 rounded-3xl flex items-center gap-4 m-2 p-2">
+    <div className="bg-slate-50 rounded-3xl flex items-center gap-2 m-2 p-2">
       <div className="flex flex-col bg-slate-100 rounded-3xl justify-center items-center w-30 h-30 p-2">
         <IoMdFlame className="text-8xl text-orange-500 drop-shadow-[0_0_15px_rgba(249,115,22,0.6)]" />
         <p className="font-bold">{input.totalDays}</p>
       </div>
       <div className="flex items-center justify-center bg-slate-100 h-30 w-full font-bold rounded-3xl ">
-        <div className="flex flex-col items-center w-9">
+        <div className="flex flex-col items-center w-8">
           <FaCircleCheck
             className={`${input.currentWeek["monday"].completed ? "text-orange-500" : ""} text-2xl`}
           />
           <p>Mo</p>
         </div>
-        <div className="flex flex-col items-center w-9">
+        <div className="flex flex-col items-center w-8">
           <FaCircleCheck
             className={`${input.currentWeek["tuesday"].completed ? "text-orange-500" : ""} text-2xl`}
           />
           <p>Di</p>
         </div>
-        <div className="flex flex-col items-center w-9">
+        <div className="flex flex-col items-center w-8">
           <FaCircleCheck
             className={`${input.currentWeek["wednesday"].completed ? "text-orange-500" : ""} text-2xl`}
           />
           <p>Mi</p>
         </div>
-        <div className="flex flex-col items-center w-9">
+        <div className="flex flex-col items-center w-8">
           <FaCircleCheck
             className={`${input.currentWeek["thursday"].completed ? "text-orange-500" : ""} text-2xl`}
           />
           <p>Do</p>
         </div>
-        <div className="flex flex-col items-center w-9">
+        <div className="flex flex-col items-center w-8">
           <FaCircleCheck
             className={`${input.currentWeek["friday"].completed ? "text-orange-500" : ""} text-2xl`}
           />
           <p>Fr</p>
         </div>
-        <div className="flex flex-col items-center w-9">
+        <div className="flex flex-col items-center w-8">
           <FaCircleCheck
             className={`${input.currentWeek["saturday"].completed ? "text-orange-500" : ""} text-2xl`}
           />
